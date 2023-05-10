61baee5b0d02926cbd66fe3647852fde7a61ca6e
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2023-05-10 12:22:47 +0200

    prima working wesrion of plots quantiles

--- CeProvoPureIo.R
+++ CeProvoPureIo.R
@@ -1,59 +1,43 @@
 library(survival)
 data("flchain")
 
-# La gammopatia monoclonale è una patologia rara, caratterizzata dall'accumulo nel midollo osseo
-# e nel sangue di una proteina anomala (paraproteina, proteina monoclonale o M proteina). --> Patologia cancerogena
-
-# Analisi fatta su campioni di sangue
-#L'elettroforesi delle sieroproteine (proteine sieriche, cioè del siero, che circolano attraverso il sistema circolatorio)
-# è un esame di screening che permette di separare le proteine del siero in base alla loro mobilità in campo elettrico.
-
-# Le molecole delle Immunoglobuline (gli anticorpi) sono proteine composte da due catene peptidiche "pesanti"
-# identiche unite a due catene "leggere" identiche.--> catene leggere kappa e lambda --> la sovrapproduzione indica malfunzionamenti
-# Il rapporto kappa/lambda per avere valori non anomali è: 0.26-1.65
-
-
-#La causa principale di questa malattia è l’alterazione di alcune cellule del midollo osseo e la loro conseguente produzione eccessiva
-#di proteina monoclonale. L’accumulo di questa proteina riduce lo spazio delle cellule sane e si concentra anche nel sangue. 
-#Nonostante non si sappiano precisamente le cause di questa alterazione, recenti ricerche sembrano
-#individuarne le cause in malattie autoimmuni (es. artrite, remautide).
-
-# Let's try something
-#### Lab 1 (leukemia) ####
-#Change into a survival variable
-
-flchain$age = as.integer(flchain$age)
-flchain$sex = as.integer(flchain$sex) 
-flchain$sample.yr = as.integer(flchain$sample.yr)
-flchain$kappa = as.integer(flchain$kappa)
-flchain$lambda = as.integer(flchain$lambda)
-flchain$flc.grp = as.integer(flchain$flc.grp)
-flchain$creatinine = as.integer(flchain$creatinine)
-flchain$mgus = as.integer(flchain$mgus)
+# QUESTION 1 - UNIVARIATE ANALYSIS ON ALL 
 
 univariate_km = function(var) {
-  surv_object <- Surv(var, death)
-  result_km <- survfit(surv_object ~ 1, conf.type = "log-log")
+  surv_object <- Surv(futime, death)
+  result_km <- survfit(surv_object ~ var, conf.type = "log-log")
   
   return(result_km)
 }
 
-#  we will not include variables: chapter, kappa, lambda (last two condensed into flc.grp)
-# variables_of_interest = c(age, sex, sample.yr, flc.grp, creatinine, mgus, futime, chapter)
+#  we will not include variables: kappa, lambda (condensed into flc.grp)
 
-age_death = univariate_km(age)
+age_quantile <- cut(age, quantile(age), include.lowest =  T)
+
+
+
+
+
+
+age_death = univariate_km(age_quantile)
 sex_death = univariate_km(sex)
 sample.yr_death = univariate_km(sample.yr)
 flc.grp_death = univariate_km(flc.grp)
 creatinine_death = univariate_km(creatinine)
 mgus_death = univariate_km(mgus)
-futime_death = univariate_km(futime)
 chapter_death = univariate_km(chapter)
 
-plot(age_death,col='black', lty = 1, xlab = "AGE", ylab = "estimated S(t)", main = 'Survival function as a function of AGE')
-plot(sex_death,col='black', lty = 1, xlab = "SEX", ylab = "estimated S(t)", main = 'Survival function as a function of SEX')
-plot(sample.yr_death,col='black', lty = 1, xlab = "SEX", ylab = "estimated S(t)", main = 'Survival function as a function of SEX')
-plot(sample.yr_death,col='black', lty = 1, xlab = "SEX", ylab = "estimated S(t)", main = 'Survival function as a function of SEX')
+plot(age_death, col = c('red', 'blue', 'black', 'green'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of age quantiles')
+legend("bottomleft", legend = c("[50,55)", "[55,63)", "[63, 72)", "[72, 101)"), lty = 1:2, col = c('red', 'blue', 'black', 'green'))
+
+
+
+plot(sample.yr_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of sample.yr')
+plot(flc.grp_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of flc.grp')
+plot(creatinine_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of creatinine')
+plot(mgus_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of mgus')
+plot(chapter_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of futime')
+
 
 # legend("topright", legend = c("treat", "control"), lty = c(1, 2),col=c("black","red"))
 
