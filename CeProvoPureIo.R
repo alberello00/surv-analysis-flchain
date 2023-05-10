f1694b71232ab4271af8b24e935fecb3b1049606
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2023-05-10 12:47:41 +0200

    primo punto fatto, solo da commentare

--- CeProvoPureIo.R
+++ CeProvoPureIo.R
@@ -1,6 +1,6 @@
 library(survival)
 data("flchain")
-
+attach(flchain)
 # QUESTION 1 - UNIVARIATE ANALYSIS ON ALL 
 
 univariate_km = function(var) {
@@ -10,43 +10,43 @@ univariate_km = function(var) {
   return(result_km)
 }
 
-#  we will not include variables: kappa, lambda (condensed into flc.grp)
+#  we will not include variables: kappa, lambda (condensed into flc.grp), and chapter (too many level)
 
+# age
 age_quantile <- cut(age, quantile(age), include.lowest =  T)
-
-
-
-
-
-
 age_death = univariate_km(age_quantile)
-sex_death = univariate_km(sex)
-sample.yr_death = univariate_km(sample.yr)
-flc.grp_death = univariate_km(flc.grp)
-creatinine_death = univariate_km(creatinine)
-mgus_death = univariate_km(mgus)
-chapter_death = univariate_km(chapter)
-
 plot(age_death, col = c('red', 'blue', 'black', 'green'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of age quantiles')
 legend("bottomleft", legend = c("[50,55)", "[55,63)", "[63, 72)", "[72, 101)"), lty = 1:2, col = c('red', 'blue', 'black', 'green'))
 
+# sample.yr
+sample.yr_death = univariate_km(sample.yr)
+plot(sample.yr_death, col = c('red', 'blue', 'black', 'green', 'red', 'blue', 'black', 'green'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of sample.yr quantiles', ylim=c(0.60,1))
+legend("bottomleft", legend = c(1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003 ), col = c('red', 'blue', 'black', 'green', 'red', 'blue', 'black', 'green'), lty = 1:2, horiz=T, cex = 0.6)
+
+# sex
+sex_death = univariate_km(sex)
+plot(sex_death, col = c('red', 'blue'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of sex', ylim=c(0.65, 1))
+legend("bottomleft", legend = c('M', 'F'), col = c('red', 'blue'), lty = 1:2, horiz = T)
 
 
-plot(sample.yr_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of sample.yr')
-plot(flc.grp_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of flc.grp')
-plot(creatinine_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of creatinine')
-plot(mgus_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of mgus')
-plot(chapter_death,col='black', lty = 1, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of futime')
+# flc.gpr
+flc.grp_death = univariate_km(flc.grp)
+plot(flc.grp_death, col = c('red', 'blue', 'black', 'green', 'purple', 'red', 'blue', 'black', 'green', 'purple'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of flc.grp', ylim=c(0.2, 1))
+legend("bottom", legend = c(1,2,3,4,5,6,7,8,9,10), col = c('red', 'blue', 'black', 'green', 'purple', 'red', 'blue', 'black', 'green', 'purple'), lty = 1:2, horiz = T, cex = 0.65)
 
 
-# legend("topright", legend = c("treat", "control"), lty = c(1, 2),col=c("black","red"))
+# creatinine
+#  [0.4,0.9] (0.9,1] (1,1.2] (1.2,10.8]
+creatinine_quanitle = cut(creatinine, quantile(creatinine, na.rm = T), include.lowest =  T)
+creatinine_death = univariate_km(creatinine_quanitle)
+plot(creatinine_death, col = c('red', 'blue', 'black', 'green'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of creatinine', ylim=c(0.4, 1))
+legend("bottomleft", legend = c("[0.4,0.9]", "(0.9,1]", "(1,1.2]", "(1.2,10.8]"), col = c('red', 'blue', 'black', 'green'), lty = 1:2, horiz = T, cex = 0.8)
 
 
-for (i in 1:8) {
-  
-}
-
-
+# mgus
+mgus_death = univariate_km(mgus)
+plot(mgus_death, col = c('red', 'blue'), lty = 1:2, xlab = "follow up time", ylab = "estimated S(t)", main = 'Survival function as a function of mgus', ylim=c(0.6, 1))
+legend("bottomleft", legend = c(0,1), col = c('red', 'blue'), lty = 1:2, horiz = T, cex = 1)
 
 
 
